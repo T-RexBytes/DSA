d721390279bd9212e4d2ee7aa0c1b1d2b946d694
TRexBytes <azurebliss12@gmail.com>
2025-10-10 20:20:20 +0000

    refactor: made it mode optimal

--- Array/largest_element.cpp
+++ Array/largest_element.cpp
@@ -5,17 +5,15 @@ int main()
     std::cout<<"Enter the size of array: ";
     std::cin>>n;
     int arr[n];
-    for(int i=0;i<n;i++)
-    {
+    for(int i=0;i<n;i++){
         std::cout<<"Enter element "<<i+1<<": ";
         std::cin>>arr[i];
     }
     int largest=arr[0];
-    for(int i=1;i<n;i++)
-    {
-        if(arr[i]>largest)
-        {
+    for(int i=1;i<n;i++){
+        if(arr[i]>largest){
             largest=arr[i];
+            break;
         }
     }
     std::cout<<largest;
