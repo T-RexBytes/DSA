f5b6e54d5924907d904c082189c1c6d22a51bf47
TRexBytes <azurebliss12@gmail.com>
2025-10-11 15:11:38 +0000

    docs : most optimal way to find the smallest element

---
+++ Array/smallest_element.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+int main()
+{
+    int n;
+    std::cout<<"Enter the size of array: ";
+    std::cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        std::cout<<"Enter element "<<i+1<<": ";
+        std::cin>>arr[i];
+    }
+    int smallest=arr[0];
+    for(int i=1;i<n;i++){
+        if(arr[i]<smallest){
+            smallest=arr[i];
+        }
+    }
+    std::cout<< "The smallest element is : " << smallest;
+    return 0;
+}
