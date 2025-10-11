e5baeab88968bd3ddaaec3ec1f96fa8e5f56d4a2
TRexBytes <azurebliss12@gmail.com>
2025-10-11 15:11:54 +0000

    docs : most optimal way to find the second smallest element

---
+++ Array/second_smallest_element.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+int main()
+{
+    int n;
+    std::cout<<"Enter the size of array: ";
+    std::cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        std::cout<<"Enter element "<<i+1<<": ";
+        std::cin>>arr[i];
+    }
+    int smallest = arr[0];
+    int second = INT_MIN;
+    for(int i=0;i<n;i++){
+        if(arr[i]<smallest){
+            second = smallest;
+            smallest = arr[i];
+        }
+        else if(arr[i]>smallest && arr[i]<second){
+            second = arr[i];
+        }
+    }
+    if(second == INT_MIN)
+        std::cout << "All the elements of the array is equal..";
+    else
+        std::cout<< "second smallest element is : " << second << std::endl;
+    return 0;
+}
