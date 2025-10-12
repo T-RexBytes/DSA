56579d1eb365a66c27191955058213a88e44d0a5
TRexBytes <azurebliss12@gmail.com>
2025-10-12 21:39:33 +0000

    feat (leetcode) : implemented max consecutive one founder

--- Array/max_consecutive_ones.cpp
+++ Array/max_consecutive_ones.cpp
@@ -4,7 +4,7 @@ public:
         int count = 0, max_count = 0;
         for (int i = 0; i < nums.size(); i++){
             if(nums[i]==1){
-                count++;
+                count+=1;
                 if(count>max_count){
                     max_count = count;
                 }
