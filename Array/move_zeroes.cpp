dfb9237adc6459b296da60646f4200fc6764bc45
TRexBytes <azurebliss12@gmail.com>
2025-10-13 16:24:48 +0000

    feat (leetcode) : implemented moving zero to the last

--- Array/move_zeroes.cpp
+++ Array/move_zeroes.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
+    void moveZeroes(std::vector<int>& nums) {
         int i = 0;
         for(int j=i;j<nums.size();j++){
             if(nums[j]!=0){
-                swap(nums[i], nums[j]);
+                std::swap(nums[i], nums[j]);
                 i++;
             }
         }
