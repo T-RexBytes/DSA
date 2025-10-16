ea43b47732b1389fca30707d2ca4d48c981a44ee
TRexBytes <azurebliss12@gmail.com>
2025-10-16 20:12:57 +0000

    feat (leetcode) : implemented duplicate remover two

---
+++ Array/duplicate_remover_two.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> dist;
+        for (int i = 0; i < nums.size(); i++) {
+            if (dist.count(nums[i]) && abs(i - dist[nums[i]]) <= k)
+                return true;
+            dist[nums[i]] = i;
+        }
+        return false;
+    }
+};
