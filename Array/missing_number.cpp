84021cea2711d0c04d83b2397eca5e50b20bf137
TRexBytes <azurebliss12@gmail.com>
2025-10-17 17:00:29 +0000

    feat (leetcode) : implemented both solutions (sum+xor) for missing element in an array

--- Array/missing_number.cpp
+++ Array/missing_number.cpp
@@ -1,12 +1,31 @@
 #include<bits/stdc++.h>
+using namespace std;
+
+// this is the sum approach
+
 class Solution {
 public:
-    int missingNumber(std::vector<int>& nums) {
+    int missingNumber(vector<int>& nums) {
         int s2 = 0;
         int s1 = (nums.size()*(nums.size()+1)/2);
         for(int i=0;i<nums.size();i++){
             s2 = s2 + nums[i];
         }
         return s1-s2;
+    }
+};
+
+// this is the xor approach
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int xor1 = 0, xor2 = 0;
+        for(int i = 0; i < nums.size(); i++){
+            xor2 ^= nums[i];   
+            xor1 ^= i;         
+        }
+        xor1 ^= nums.size();             
+        return xor1 ^ xor2;    
     }
 };
