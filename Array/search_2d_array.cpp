f9646c59a3a7396a4e2acbfb188dc301c95f2c80
TRexBytes <azurebliss12@gmail.com>
2025-10-23 23:55:45 +0530

    feat(leetcode) : implemented searching in 2d array

--- Array/search_2d_array.cpp
+++ Array/search_2d_array.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size();        // number of rows
-        int n = matrix[0].size();     // number of columns
-        
-        for (int i = 0; i < m; i++) {
-            // check the last element of each row
-            if (matrix[i][n - 1] == target)
-                return true;
-
-            // if the last element is greater than target, search in this row
-            else if (matrix[i][n - 1] > target) {
-                for (int j = 0; j < n; j++) {
-                    if (matrix[i][j] == target)
-                        return true;
+        if (matrix.empty() || matrix[0].empty()) return false;
+        // Find the possible row
+        for (int i = 0; i < matrix.size(); i++) {
+            if (target <= matrix[i][matrix[0].size() - 1]) {
+                // Search in that row
+                for (int j = 0; j < matrix[0].size(); j++) {
+                    if (matrix[i][j] == target) return true;
                 }
-                return false; // target not in this row
+                return false;
             }
         }
-
-        return false; // target not found in any row
+        return false;
     }
 };
+
