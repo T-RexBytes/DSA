9556e92d267013fed841b8fe1bb2fbcb583d0d97
TRexBytes <azurebliss12@gmail.com>
2025-10-25 01:34:14 +0530

    feat(leetcode) : implemented finding even number of digit in a array element

---
+++ Array/find_even_numbers_of_digits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int count = 0;
+      
+        for (int n : nums) {
+            int digits = (n == 0) ? 1 : (int)log10(abs(n)) + 1;
+            if (digits % 2 == 0) count++;
+        }
+      
+        return count;
+    }
+};
