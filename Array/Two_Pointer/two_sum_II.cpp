03e620951d64cd0acaa25ef1df7e556ce7721480
TRexBytes <azurebliss12@gmail.com>
2025-10-27 00:37:58 +0530

    feat(leetcode): Implemented Two Sum II function using two-pointer technique

--- Array/Two_Pointer/two_sum_II.cpp
+++ Array/Two_Pointer/two_sum_II.cpp
@@ -1,1 +1,19 @@
-
+//PB 00:01:05
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i = 0, j = (int)numbers.size()-1;
+        while(i < j){
+            if(numbers[i]+numbers[j] == target){
+                return {i+1, j+1};
+            }
+            if(numbers[i]+numbers[j] > target){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return {};
+    }
+};
