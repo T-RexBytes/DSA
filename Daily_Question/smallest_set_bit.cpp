9dd629b2d7143b06e0fc0fe7357915a3be72fe9a
TRexBytes <azurebliss12@gmail.com>
2025-10-29 22:25:10 +0530

    feat(leetcode): implemented smallest number with all set bit

---
+++ Daily_Question/smallest_set_bit.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int smallestNumber(int n) {
+    int bits=floor(log2(n))+1;  //this gives the closest bit length of set bit  
+        int ans=1<<bits;    // this is the same as (2^bits)-1
+        return ans-1; 
+    }
+};
