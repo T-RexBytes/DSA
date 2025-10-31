eb56cbd9ff77233337ec1152a040e3431ed367fb
TRexBytes <azurebliss12@gmail.com>
2025-10-31 23:55:21 +0530

    Day 1 feat(leetcode): implemented container with most water

--- Daily_Question/smallest_set_bit.cpp
+++ Daily_Question/smallest_set_bit.cpp
@@ -3,6 +3,6 @@ public:
     int smallestNumber(int n) {
     int bits=floor(log2(n))+1;  //this gives the closest bit length of set bit  
         int ans=1<<bits;    // this is the same as (2^bits)-1
-        return ans-1; 
+        return ans-1;  
     }
 };
