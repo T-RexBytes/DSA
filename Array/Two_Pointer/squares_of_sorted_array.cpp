aece388d66ce650a39e3005f90bcccdc96de3eb7
TRexBytes <azurebliss12@gmail.com>
2025-11-03 22:09:00 +0530

    feat(leetcode): implemented squares of sorted array

---
+++ Array/Two_Pointer/squares_of_sorted_array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> res(nums.size());
+        int left = 0, right = nums.size() - 1, pos = nums.size() - 1;
+        
+        while (left <= right) {
+            if (abs(nums[left]) > abs(nums[right])) {
+                res[pos--] = nums[left] * nums[left];
+                left++;
+            } else {
+                res[pos--] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return res;
+    }
+};
