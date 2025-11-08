280456162749c2ad0b265a9f22ae07a6553d4be2
TRexBytes <azurebliss12@gmail.com>
2025-11-08 23:56:28 +0530

    Refactor threeSum function for clarity
    Refactor the threeSum function to improve readability and maintainability.

--- Array/Two_Pointer/3_sum.cpp
+++ Array/Two_Pointer/3_sum.cpp
@@ -3,26 +3,22 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums){
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        vector<vector<int>>lol; //this needs to be optimised
-        for(int i=0;i<n;i++)
-        {
+        vector<vector<int>>lol;
+        for(int i=0;i<n;i++){
             if(nums[i]>0) break;
             if(i>0 && nums[i]==nums[i-1]) continue;
             int j=i+1,k=n-1;
-            while(j<k)
-            {
+            while(j<k){
                int m=nums[i]+nums[j]+nums[k];
                if(m>0)
                     k--;
                 else if(m<0)
                     j++;
-                else
-                {
+                else{
                     lol.push_back({nums[i],nums[j],nums[k]});
                     j++;
                     k--;
-                    while(j<k && nums[j]==nums[j-1])
-                    {
+                    while(j<k && nums[j]==nums[j-1]){
                        j++;
                     }
                 }
