c5bb5a882bd3684ad5a95dd0a7d76910693512db
TRexBytes <azurebliss12@gmail.com>
2025-11-14 22:41:23 +0530

    feat(leetcode): implemeted making array element equals to zero

--- Array/array_element_equal_zero.cpp
+++ Array/array_element_equal_zero.cpp
@@ -3,20 +3,13 @@ public:
     int countValidSelections(vector<int>& nums) {
         int n = nums.size();
         int total = 0;
-        
-
         for (int x : nums) total += x;
-        
         int valid = 0;
         int leftSum = 0;
-        
-  
         for (int i = 0; i < n; i++) {
             if (nums[i] == 0) {
                 int rightSum = total - leftSum;
-                int diff = abs(leftSum - rightSum);
-                
-                
+                int diff = abs(leftSum - rightSum);             
                 if (diff == 0) {
                     valid += 2;
                 } else if (diff == 1) {
@@ -25,7 +18,6 @@ public:
             }
             leftSum += nums[i];
         }
-        
         return valid;
     }
 };
