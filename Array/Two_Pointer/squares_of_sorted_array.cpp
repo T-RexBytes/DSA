46ab934613ec8664aae918410696881a941daf51
TRexBytes <azurebliss12@gmail.com>
2025-11-16 23:18:34 +0530

    this needs to be updated

--- Array/Two_Pointer/squares_of_sorted_array.cpp
+++ Array/Two_Pointer/squares_of_sorted_array.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> sortedSquares(vector<int>& nums) {
         vector<int> res(nums.size());
         int left = 0, right = nums.size() - 1, pos = nums.size() - 1;
-        
+        //this needs to be updated
         while (left <= right) {
             if (abs(nums[left]) > abs(nums[right])) {
                 res[pos--] = nums[left] * nums[left];
