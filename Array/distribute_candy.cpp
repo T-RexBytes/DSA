344181d8e5f7f9c251f83a8c917509ab947598f7
TRexBytes <azurebliss12@gmail.com>
2025-11-18 23:59:41 +0530

    feat(leetcode): implemented distributeCandies

--- Array/distribute_candy.cpp
+++ Array/distribute_candy.cpp
@@ -3,7 +3,8 @@ public:
     int distributeCandies(vector<int>& candyType) {
        unordered_set<int> st;
        for(int i : candyType){
-        if(st.size() ==  candyType.size()/2) return  candyType.size()/2;
+        if(st.size() == candyType.size()/2){
+            return  candyType.size()/2;
         st.insert(i);
        }
        return st.size();
