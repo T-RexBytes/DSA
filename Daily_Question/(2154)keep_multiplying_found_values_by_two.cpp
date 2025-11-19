5d057f06231a950964735cfcbb404d4346856e46
TRexBytes <azurebliss12@gmail.com>
2025-11-20 02:07:35 +0530

    Add solution for multiplying found values by two
    Implement a solution to keep multiplying found values by two.

---
+++ Daily_Question/(2154)keep_multiplying_found_values_by_two.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        int i = 0;
+        while(i<nums.size()){       //while loop is used cause the it will only be looped when needed not like for loop
+            if(nums[i]==original){  // if original is found in nums it will get doubled and the i will be set as 0
+                original*=2;
+                i=0;                //this means the loop will be starting from the first again
+            }
+            else{
+                i++;
+            }
+        }
+        return original;
+    }
+};
