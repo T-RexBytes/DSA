fc722edfd3e113ea582473a69f9f34d3e5803578
TRexBytes <azurebliss12@gmail.com>
2025-11-24 23:59:29 +0530

    Create hello.cpp

---
+++ Daily_Question/hello.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& nums) {
+        vector<bool> ans;
+        ans.reserve(nums.size());
+
+        int mod = 0;
+
+        for (int bit : nums) {
+            mod = ((mod << 1) + bit) % 5;   // keep only mod 5
+            ans.push_back(mod == 0);
+        }
+
+        return ans;
+    }
+};
