6230d44101f13434c1c2c8b2532c8d6c60b2a7bf
TRexBytes <azurebliss12@gmail.com>
2025-11-29 23:55:25 +0530

    Update paths_in_matrix_which_adds_upto_k.cpp

--- Daily_Question/paths_in_matrix_which_adds_upto_k.cpp
+++ Daily_Question/paths_in_matrix_which_adds_upto_k.cpp
@@ -1,36 +1,23 @@
-// need to be updated
-
+// variant with better initialization
 class Solution {
 public:
-    static const int MOD = 1e9 + 7;
-
-    int numberOfPaths(vector<vector<int>>& grid, int k) {
-        int m = grid.size(), n = grid[0].size();
-
-        // dp[j][r] → compressed 2D DP (row by row)
-        vector<vector<int>> dp(n, vector<int>(k, 0));
-
-        dp[0][grid[0][0] % k] = 1;
-
-        for (int i = 0; i < m; i++) {
-            vector<vector<int>> new_dp(n, vector<int>(k, 0));
-            for (int j = 0; j < n; j++) {
-                for (int r = 0; r < k; r++) {
-                    if (dp[j][r] == 0) continue;
-
-                    int new_r = (r + grid[i][j]) % k;
-
-                    // From top
-                    new_dp[j][new_r] = (new_dp[j][new_r] + dp[j][r]) % MOD;
-
-                    // From left (only if j > 0)
-                    if (j > 0)
-                        new_dp[j][new_r] = (new_dp[j][new_r] + new_dp[j-1][r]) % MOD;
+    static int numberOfPaths(vector<vector<int>>& grid, int k) {
+        const int m=grid.size(), n=grid[0].size();
+        const int mod=1e9+7;
+        int dp[2][n][k];
+        memset(dp, 0, sizeof(dp));
+        dp[0][0][(k-grid[0][0]%k)%k]=1;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                const int x=grid[i][j];
+                for(int r=0; r<k; r++){
+                    const int R0=(r+x)%k;
+                    if (i>0) dp[i&1][j][r]=dp[(i-1)&1][j][R0];
+                    if (j>0) dp[i&1][j][r]+=dp[i&1][j-1][R0];
+                    dp[i&1][j][r]%=mod;
                 }
             }
-            dp = new_dp;
         }
-
-        return dp[n-1][0];
+        return dp[(m-1)&1][n-1][0];
     }
 };
