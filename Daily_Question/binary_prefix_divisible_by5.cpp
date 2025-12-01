965e36a24432d1b1b4fbbae331cfdb9b3c992ace
TRexBytes <azurebliss12@gmail.com>
2025-12-02 00:00:09 +0530

    Update binary_prefix_divisible_by5.cpp

--- Daily_Question/binary_prefix_divisible_by5.cpp
+++ Daily_Question/binary_prefix_divisible_by5.cpp
@@ -3,9 +3,7 @@ public:
     vector<bool> prefixesDivBy5(vector<int>& nums) {
         vector<bool> ans;
         ans.reserve(nums.size());
-
         int mod = 0;
-
         for (int bit : nums) {
             mod = ((mod << 1) + bit) % 5;   // keep only mod 5
             ans.push_back(mod == 0);
